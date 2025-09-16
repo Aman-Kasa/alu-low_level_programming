fcc0129d71ea9a68b9c4dae1d6d732d2cc22a6fa
Aman-Kasa <a.kasa@alustudent.com>
2025-09-16 18:39:51 +0200

    corrected 3-main

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,10 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-
-#endif
-
-
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -17,4 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
-int _putchar(char c);
+
+#endif
