442590c00bd419f7b52ab0130bb925eac7b77a83
Aman-Kasa <a.kasa@alustudent.com>
2025-09-16 23:00:50 +0200

    Add 102-fibonacci.c: print first 50 Fibonacci numbers

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -1,25 +1,24 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - prints first 50 Fibonacci numbers, starting with 1 and 2
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
  *
  * Return: Always 0
  */
 int main(void)
 {
-    long a = 1, b = 2, c;
-    int i;
+	unsigned long a = 1, b = 2, next;
+	int count;
 
-    printf("%ld, %ld", a, b);
-
-    for (i = 3; i <= 50; i++)
-    {
-        c = a + b;
-        printf(", %ld", c);
-        a = b;
-        b = c;
-    }
-
-    printf("\n");
-    return (0);
+	printf("%lu, %lu", a, b);
+	for (count = 3; count <= 50; count++)
+	{
+		next = a + b;
+		printf(", %lu", next);
+		a = b;
+		b = next;
+	}
+	printf("\n");
+	return (0);
 }
