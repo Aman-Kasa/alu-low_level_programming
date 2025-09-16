3d0cec37f39eff3ea55c2ce6459f5f37a51efab8
Aman-Kasa <a.kasa@alustudent.com>
2025-09-16 22:45:16 +0200

    Add 8-24_hours.c with jack_bauer and dedicated main

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -14,4 +14,4 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 
-#endif
+#endif /* MAIN_H */
---
+++ functions_nested_loops/main_8.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - test jack_bauer function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    jack_bauer();
+    return (0);
+}
