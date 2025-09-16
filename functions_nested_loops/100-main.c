8aeab7a5f9f2c47250c7a751b9a82f32712ff1e9
Aman-Kasa <a.kasa@alustudent.com>
2025-09-16 22:58:43 +0200

    100 and 101

---
+++ functions_nested_loops/100-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code for print_times_table
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_times_table(2);
+	_putchar('\n');
+	print_times_table(10);
+	_putchar('\n');
+	print_times_table(13);
+	_putchar('\n');
+	print_times_table(-2);
+	_putchar('\n');
+	print_times_table(0);
+	_putchar('\n');
+	print_times_table(100);
+	_putchar('\n');
+	return (0);
+}
