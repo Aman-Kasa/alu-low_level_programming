acddf1a682fa199eda3734696b2cef393487b952
Aman-Kasa <a.kasa@alustudent.com>
2025-09-16 23:10:17 +0200

    fibonanci 104 corr output

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,40 +1,43 @@
 #include <stdio.h>
 
 /**
- * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
- *
+ * main - prints the first 98 Fibonacci numbers, separated by comma and space
  * Return: Always 0
  */
 int main(void)
 {
-	unsigned long f1 = 1, f2 = 2, next;
-	unsigned long f1_h = 0, f2_h = 0, next_h;
+	unsigned long f1 = 1, f2 = 2;
+	unsigned long f1_h = 0, f1_l = 1;
+	unsigned long f2_h = 0, f2_l = 2;
+	unsigned long next_h, next_l;
 	int i;
 
 	printf("%lu, %lu", f1, f2);
+
 	for (i = 3; i <= 98; i++)
 	{
-		/* Add low parts */
-		next = f1 + f2;
-		next_h = f1_h + f2_h;
-
-		/* Handle overflow of unsigned long (simulate high part) */
-		if (next < f1)
+		if (f2 < 1000000000UL)
 		{
-			next_h += 1;
-		}
-
-		/* Print depending if high part is zero or not */
-		if (next_h == 0)
+			unsigned long next = f1 + f2;
 			printf(", %lu", next);
+			f1 = f2;
+			f2 = next;
+		}
 		else
-			printf(", %lu%09lu", next_h, next);
-
-		/* Shift numbers */
-		f1 = f2;
-		f1_h = f2_h;
-		f2 = next;
-		f2_h = next_h;
+		{
+			next_l = f1_l + f2_l;
+			next_h = f1_h + f2_h;
+			if (next_l >= 1000000000UL)
+			{
+				next_l -= 1000000000UL;
+				next_h += 1;
+			}
+			printf(", %lu%09lu", next_h, next_l);
+			f1_l = f2_l;
+			f1_h = f2_h;
+			f2_l = next_l;
+			f2_h = next_h;
+		}
 	}
 	printf("\n");
 	return (0);
