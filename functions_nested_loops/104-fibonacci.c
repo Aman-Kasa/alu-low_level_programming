2d30cca73734ef24459a9c8b700fcf792fdaa0b4
Aman-Kasa <a.kasa@alustudent.com>
2025-09-22 23:01:26 +0200

    xz

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -2,43 +2,48 @@
 
 /**
  * main - prints the first 98 Fibonacci numbers, separated by comma and space
+ *
  * Return: Always 0
  */
 int main(void)
 {
-	unsigned long f1 = 1, f2 = 2;
-	unsigned long f1_h = 0, f1_l = 1;
-	unsigned long f2_h = 0, f2_l = 2;
-	unsigned long next_h, next_l;
+	unsigned long f1 = 0, f2 = 1, f3;
+	unsigned long f1_h, f1_l, f2_h, f2_l;
+	unsigned long h1, h2;
 	int i;
 
-	printf("%lu, %lu", f1, f2);
-
-	for (i = 3; i <= 98; i++)
+	for (i = 1; i <= 92; i++) /* first 92 fit in unsigned long */
 	{
-		if (f2 < 1000000000UL)
-		{
-			unsigned long next = f1 + f2;
-			printf(", %lu", next);
-			f1 = f2;
-			f2 = next;
-		}
-		else
-		{
-			next_l = f1_l + f2_l;
-			next_h = f1_h + f2_h;
-			if (next_l >= 1000000000UL)
-			{
-				next_l -= 1000000000UL;
-				next_h += 1;
-			}
-			printf(", %lu%09lu", next_h, next_l);
-			f1_l = f2_l;
-			f1_h = f2_h;
-			f2_l = next_l;
-			f2_h = next_h;
-		}
+		f3 = f1 + f2;
+		printf("%lu", f3);
+		if (i != 92)
+			printf(", ");
+		f1 = f2;
+		f2 = f3;
 	}
+
+	f1_h = f1 / 10000000000UL;
+	f1_l = f1 % 10000000000UL;
+	f2_h = f2 / 10000000000UL;
+	f2_l = f2 % 10000000000UL;
+
+	for (; i <= 98; i++)
+	{
+		h1 = f1_h + f2_h;
+		h2 = f1_l + f2_l;
+		if (h2 >= 10000000000UL)
+		{
+			h1 += 1;
+			h2 -= 10000000000UL;
+		}
+		printf(", %lu%010lu", h1, h2);
+
+		f1_h = f2_h;
+		f1_l = f2_l;
+		f2_h = h1;
+		f2_l = h2;
+	}
+
 	printf("\n");
 	return (0);
 }
