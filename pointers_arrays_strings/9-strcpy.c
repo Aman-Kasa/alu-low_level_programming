e2470eefdca04ee8bce72ab8d18bf5c16fcf4e3a
Aman-Kasa <a.kasa@alustudent.com>
2025-09-29 19:52:55 +0200

    Add task 9: _strcpy function and main

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,20 +2,20 @@
 
 /**
  * _strcpy - copies the string pointed to by src
- * @dest: destination buffer
- * @src: source string
+ * @dest: pointer to the buffer where string is copied
+ * @src: pointer to the source string
  *
  * Return: pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
-    int i = 0;
+	int i = 0;
 
-    while (src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-    return (dest);
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
