a2c1fd99504f9514ae0711edd1ff6228e7172219
Aman-Kasa <a.kasa@alustudent.com>
2025-09-29 19:47:50 +0200

    Add task 7: puts_half function and main

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string
- * @str: string to print
+ * puts2 - prints every other character of a string, starting with the first
+ * @str: pointer to the string
  *
  * Return: nothing
  */
 void puts2(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        if (i % 2 == 0)
-            _putchar(str[i]);
-        i++;
-    }
-    _putchar('\n');
+	while (*(str + i) != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
 }
--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -2,22 +2,24 @@
 
 /**
  * puts_half - prints the second half of a string
- * @str: string to print
+ * @str: pointer to the string
  *
  * Return: nothing
  */
 void puts_half(char *str)
 {
-    int len = 0, i;
+	int len = 0, i;
 
-    while (str[len] != '\0')
-        len++;
+	while (*(str + len) != '\0')
+		len++;
 
-    i = len / 2;
-    if (len % 2 != 0)
-        i = (len + 1) / 2;
+	if (len % 2 == 0)
+		i = len / 2;
+	else
+		i = (len + 1) / 2;
 
-    for (; i < len; i++)
-        _putchar(str[i]);
-    _putchar('\n');
+	for (; i < len; i++)
+		_putchar(*(str + i));
+
+	_putchar('\n');
 }
