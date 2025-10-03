13100b83552b3f760d20b22834f6e0f17831c535
Aman-Kasa <a.kasa@alustudent.com>
2025-10-03 19:06:19 +0200

    Add 101-keygen: seed-aware generator matching test_crackme

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,4 @@
-/* 101-keygen.c - key generator that accepts optional seed (C90) */
+/* 101-keygen.c - seed-aware keygen (C90) */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,6 +8,7 @@ int main(int argc, char **argv)
     const char *pool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     unsigned int seed;
     int pool_len = 62;
+    char key[9];
     int i;
 
     if (argc == 2)
@@ -15,12 +16,11 @@ int main(int argc, char **argv)
     else
         seed = (unsigned int)time(NULL);
 
-    printf("Seed: %u\n", seed);
     srand(seed);
-
     for (i = 0; i < 8; i++)
-        putchar(pool[rand() % pool_len]);
-    putchar('\n');
+        key[i] = pool[rand() % pool_len];
+    key[8] = '\0';
+    printf("%s\n", key);
 
     return (0);
 }
