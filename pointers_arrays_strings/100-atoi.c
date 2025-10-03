5760885611ae378ad317ad5484720e1833c21e5c
Aman-Kasa <a.kasa@alustudent.com>
2025-10-03 17:26:32 +0200

    betty style code fixed

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,34 +1,40 @@
 #include "main.h"
 
 /**
- * _atoi - Converts a string to an integer.
- * @s: The input string.
+ * _atoi - converts a string to an integer
+ * @s: input string
  *
- * Return: The integer value represented in the string.
+ * Return: converted integer
  */
 int _atoi(char *s)
 {
-    int i = 0, sign = 1, num = 0;
+	int i = 0, sign = 1, num = 0;
+	int digit;
 
-    /* Skip non-digit characters, handle signs */
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        else if (s[i] == '+')
-            ; /* do nothing, sign unchanged */
-        else if (s[i] >= '0' && s[i] <= '9')
-            break;
-        i++;
-    }
+	while (s[i])
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] == '+')
+			;
+		else if (s[i] >= '0' && s[i] <= '9')
+			break;
+		i++;
+	}
 
-    /* Convert digits to integer */
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        num = num * 10 + (s[i] - '0');
-        i++;
-    }
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		digit = s[i] - '0';
 
-    return (num * sign);
+		if (sign == 1 && num > (2147483647 - digit) / 10)
+			return (2147483647);
+		if (sign == -1 && num > (2147483648 - digit) / 10)
+			return (-2147483648);
+
+		num = num * 10 + digit;
+		i++;
+	}
+
+	return (num * sign);
 }
 
