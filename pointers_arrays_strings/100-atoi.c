edf20530654d9d09579b69ebb9885d4e742d7a51
Aman-Kasa <a.kasa@alustudent.com>
2025-10-03 17:24:25 +0200

    main code header added

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,44 +1,34 @@
 #include "main.h"
-#include <limits.h>
 
 /**
- * _atoi - convert a string to an integer
- * @s: string to convert
- * Return: integer value or 0
+ * _atoi - Converts a string to an integer.
+ * @s: The input string.
+ *
+ * Return: The integer value represented in the string.
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, result = 0;
-	int found_digit = 0;
+    int i = 0, sign = 1, num = 0;
 
-	/* Skip non-digit characters, handle signs */
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			found_digit = 1;
-			break;
-		}
-		i++;
-	}
+    /* Skip non-digit characters, handle signs */
+    while (s[i] != '\0')
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        else if (s[i] == '+')
+            ; /* do nothing, sign unchanged */
+        else if (s[i] >= '0' && s[i] <= '9')
+            break;
+        i++;
+    }
 
-	if (!found_digit)
-		return (0);
+    /* Convert digits to integer */
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        num = num * 10 + (s[i] - '0');
+        i++;
+    }
 
-	/* Convert digits */
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		int digit = s[i] - '0';
-
-		/* Check overflow */
-		if (result > (INT_MAX - digit) / 10)
-			return (sign == 1 ? INT_MAX : INT_MIN);
-
-		result = result * 10 + digit;
-		i++;
-	}
-
-	return (result * sign);
+    return (num * sign);
 }
+
