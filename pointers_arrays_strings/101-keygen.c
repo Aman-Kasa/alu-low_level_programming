a24a718f55f87cc36bb88cffe58fb8ba5680410f
Aman-Kasa <a.kasa@alustudent.com>
2025-10-03 18:43:34 +0200

    corrected: char

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,15 @@
-/* 101-keygen_time_alnum.c */
+/* 101-keygen_time_pid.c */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 int main(void)
 {
     const char *pool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    int poollen = 62;
-    int i;
+    int poollen = 62, i;
 
-    srand((unsigned int)time(NULL));
+    srand((unsigned int)(time(NULL) ^ (getpid() << 16)));
     for (i = 0; i < 8; i++)
         putchar(pool[rand() % poollen]);
     putchar('\n');
