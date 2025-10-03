bbfb7cfc36d8231965ac578f5ff96ee552d0044d
Aman-Kasa <a.kasa@alustudent.com>
2025-10-03 18:45:24 +0200

    corr char

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,18 +1,25 @@
-/* 101-keygen_time_pid.c */
+/* brute-keygen.c - generate candidate for each seed in window */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <unistd.h>
 
-int main(void)
-{
+void make_key(unsigned int seed, char *out) {
     const char *pool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     int poollen = 62, i;
-
-    srand((unsigned int)(time(NULL) ^ (getpid() << 16)));
+    srand(seed);
     for (i = 0; i < 8; i++)
-        putchar(pool[rand() % poollen]);
-    putchar('\n');
+        out[i] = pool[rand() % poollen];
+    out[8] = '\0';
+}
+
+int main(void) {
+    unsigned int now = (unsigned int)time(NULL);
+    char key[9];
+    int offset;
+    for (offset = -5; offset <= 5; offset++) {
+        make_key(now + offset, key);
+        printf("seed %u -> %s\n", now + offset, key);
+    }
     return 0;
 }
 
