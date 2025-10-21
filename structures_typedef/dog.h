c6b0b1b0401f75c31fc4ea0d1fd214324c47d363
Aman-Kasa <a.kasa@alustudent.com>
2025-10-21 03:12:36 +0200

    dog_t new name for struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,35 +16,16 @@ struct dog
 	char *owner;
 };
 
-/* Task 1: initialize a dog
-  init_dog - initializes a variable of type struct dog
-  @d: pointer to the struct dog to initialize
-  @name: name of the dog
-  @age: age of the dog
-  @owner: owner of the dog*/
+/* Task 1: initialize a dog */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
-/* Task 2: print a dog 
-  print_dog - prints a struct dog
-  @d: pointer to struct dog
- 
-  If an element of d is NULL, print (nil) instead.
-  If d is NULL, print nothing.*/
+/* Task 2: print a dog */
 void print_dog(struct dog *d);
 
-/* Task 3: typedef dog_t as alias for struct dog 
-  new_dog - creates a new dog
-  @name: name of the dog
-  @age: age of the dog
-  @owner: owner of the dog
- 
-  Return: pointer to the new dog, or NULL on failure*/
+/* Task 3: typedef dog_t as alias for struct dog */
 typedef struct dog dog_t;
 
-/* Task 4: create a new dog (allocates memory) 
-  free_dog - frees memory allocated for a dog
-  @d: pointer to the dog to free
- */
+/* Task 4: create a new dog (allocates memory) */
 dog_t *new_dog(char *name, float age, char *owner);
 
 /* Task 5: free a dog */
