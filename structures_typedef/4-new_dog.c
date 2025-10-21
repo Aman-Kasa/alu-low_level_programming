ead612133d1e6f0d4f702df7703d94264c9be4b0
Aman-Kasa <a.kasa@alustudent.com>
2025-10-21 04:47:18 +0200

    betty style corrected and strlen;;strcp removed

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,36 +11,36 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *d;
-    char *name_copy, *owner_copy;
-    int i, len;
+	dog_t *d;
+	char *name_copy, *owner_copy;
+	int i, len;
 
     /* Allocate memory for dog structure */
-    d = malloc(sizeof(dog_t));
-    if (!d)
-        return (NULL);
+	d = malloc(sizeof(dog_t));
+	if (!d)
+	return (NULL);
 
     /* Allocate and copy name */
-    for (len = 0; name[len] != '\0'; len++)
-        ;
-    name_copy = malloc(len + 1);
-    if (!name_copy)
+	for (len = 0; name[len] != '\0'; len++);
+
+	name_copy = malloc(len + 1);
+	if (!name_copy)
     {
-        free(d);
-        return (NULL);
+	free(d);
+	return (NULL);
     }
-    for (i = 0; i <= len; i++)
-        name_copy[i] = name[i];
+	for (i = 0; i <= len; i++)
+	name_copy[i] = name[i];
 
     /* Allocate and copy owner */
-    for (len = 0; owner[len] != '\0'; len++)
-        ;
-    owner_copy = malloc(len + 1);
-    if (!owner_copy)
+	for (len = 0; owner[len] != '\0'; len++);
+
+	owner_copy = malloc(len + 1);
+	if (!owner_copy)
     {
-        free(name_copy);
-        free(d);
-        return (NULL);
+	free(name_copy);
+	free(d);
+	return (NULL);
     }
     for (i = 0; i <= len; i++)
         owner_copy[i] = owner[i];
