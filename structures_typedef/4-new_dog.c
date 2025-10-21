02c1a4b47571afe196b0c7681ea00db402dcd12b
Aman-Kasa <a.kasa@alustudent.com>
2025-10-21 04:42:11 +0200

    betty style corrected and strlen;;strcp removed

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,47 +1,70 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
- * new_dog - creates a new dog
+ * new_dog - Creates a new dog structure
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  *
- * Return: pointer to the new dog, or NULL if it fails
+ * Return: pointer to the new dog, or NULL if malloc fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
-	char *new_name;
-	char *new_owner;
+    dog_t *d;
+    char *name_copy, *owner_copy;
+    int i, len;
 
-	d = malloc(sizeof(dog_t));
-	if (d == NULL)
-		return (NULL);
+    /* Allocate memory for dog structure */
+    d = malloc(sizeof(dog_t));
+    if (!d)
+        return (NULL);
 
-	new_name = malloc(strlen(name) + 1);
-	if (new_name == NULL)
-	{
-		free(d);
-		return (NULL);
-	}
-	strcpy(new_name, name);
+    /* Allocate and copy name */
+    for (len = 0; name[len] != '\0'; len++)
+        ;
+    name_copy = malloc(len + 1);
+    if (!name_copy)
+    {
+        free(d);
+        return (NULL);
+    }
+    for (i = 0; i <= len; i++)
+        name_copy[i] = name[i];
 
-	new_owner = malloc(strlen(owner) + 1);
-	if (new_owner == NULL)
-	{
-		free(new_name);
-		free(d);
-		return (NULL);
-	}
-	strcpy(new_owner, owner);
+    /* Allocate and copy owner */
+    for (len = 0; owner[len] != '\0'; len++)
+        ;
+    owner_copy = malloc(len + 1);
+    if (!owner_copy)
+    {
+        free(name_copy);
+        free(d);
+        return (NULL);
+    }
+    for (i = 0; i <= len; i++)
+        owner_copy[i] = owner[i];
 
-	d->name = new_name;
-	d->age = age;
-	d->owner = new_owner;
+    /* Set dog fields */
+    d->name = name_copy;
+    d->age = age;
+    d->owner = owner_copy;
 
-	return (d);
+    return (d);
+}
+/**
+ * free_dog - frees memory allocated for a dog
+ * @d: pointer to dog to free
+ */
+void free_dog(dog_t *d)
+{
+    if (d)
+    {
+        if (d->name)
+            free(d->name);
+        if (d->owner)
+            free(d->owner);
+        free(d);
+    }
 }
 
