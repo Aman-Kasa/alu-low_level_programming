d63a43541829cf3ad18ccc936c20863841f4e453
Aman-Kasa <a.kasa@alustudent.com>
2025-10-21 04:49:19 +0200

    betty style corrected and strlen;;strcp removed

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,56 +15,59 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *name_copy, *owner_copy;
 	int i, len;
 
-    /* Allocate memory for dog structure */
+	/* Allocate memory for dog structure */
 	d = malloc(sizeof(dog_t));
 	if (!d)
-	return (NULL);
+		return (NULL);
 
-    /* Allocate and copy name */
-	for (len = 0; name[len] != '\0'; len++);
-
+	/* Allocate and copy name */
+	len = 0;
+	while (name[len] != '\0')
+		len++;
 	name_copy = malloc(len + 1);
 	if (!name_copy)
-    {
-	free(d);
-	return (NULL);
-    }
+	{
+		free(d);
+		return (NULL);
+	}
 	for (i = 0; i <= len; i++)
-	name_copy[i] = name[i];
+		name_copy[i] = name[i];
 
-    /* Allocate and copy owner */
-	for (len = 0; owner[len] != '\0'; len++);
-
+	/* Allocate and copy owner */
+	len = 0;
+	while (owner[len] != '\0')
+		len++;
 	owner_copy = malloc(len + 1);
 	if (!owner_copy)
-    {
-	free(name_copy);
-	free(d);
-	return (NULL);
-    }
-    for (i = 0; i <= len; i++)
-        owner_copy[i] = owner[i];
+	{
+		free(name_copy);
+		free(d);
+		return (NULL);
+	}
+	for (i = 0; i <= len; i++)
+		owner_copy[i] = owner[i];
 
-    /* Set dog fields */
-    d->name = name_copy;
-    d->age = age;
-    d->owner = owner_copy;
+	/* Set dog fields */
+	d->name = name_copy;
+	d->age = age;
+	d->owner = owner_copy;
 
-    return (d);
+	return (d);
 }
+
 /**
  * free_dog - frees memory allocated for a dog
  * @d: pointer to dog to free
  */
 void free_dog(dog_t *d)
 {
-    if (d)
-    {
-        if (d->name)
-            free(d->name);
-        if (d->owner)
-            free(d->owner);
-        free(d);
-    }
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
 }
 
