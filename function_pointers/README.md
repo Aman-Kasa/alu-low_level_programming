2e2d5be2b8d836abd38fa7b419d64d9a58422576
Aman-Kasa <a.kasa@alustudent.com>
2025-11-06 22:42:49 +0200

    0-print_name added

---
+++ function_pointers/README.md
@@ -0,0 +1,1 @@
+#fuctions_pointer
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/* Prototype for _putchar (provided by the checker) */
+int _putchar(char c);
+
+/* Prototype for print_name function */
+void print_name(char *name, void (*f)(char *));
+
+#endif /* FUNCTION_POINTERS_H */
+
