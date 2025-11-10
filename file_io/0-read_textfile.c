eeedf1583516391055e7ee58f706f65f8b678f84
Aman-Kasa <a.kasa@alustudent.com>
2025-11-10 19:01:27 +0200

    betty style for 0-read_textfile.c added

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,43 +13,42 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t n_read, n_written;
-    char *buffer;
+	int fd;
+	ssize_t n_read, n_written;
+	char *buffer;
 
-    if (!filename || letters == 0)
-        return (0);
+	if (!filename || letters == 0)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (!buffer)
-    {
-        close(fd);
-        return (0);
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+	{
+		close(fd);
+		return (0);
+	}
 
-    n_read = read(fd, buffer, letters);
-    if (n_read == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    n_written = write(STDOUT_FILENO, buffer, n_read);
-    if (n_written != n_read)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	n_written = write(STDOUT_FILENO, buffer, n_read);
+	if (n_written != n_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    free(buffer);
-    close(fd);
-
-    return (n_written);
+	free(buffer);
+	close(fd);
+	return (n_written);
 }
 
