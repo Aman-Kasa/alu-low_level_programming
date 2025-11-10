598c4773076d96913e101696d09c8d5a230d4cf9
Aman-Kasa <a.kasa@alustudent.com>
2025-11-10 19:06:39 +0200

    2 append text to file

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: the name of the file
+ * @text_content: NULL terminated string to add at the end
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t len, written;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		len = strlen(text_content);
+		written = write(fd, text_content, len);
+		if (written != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
+
--- file_io/main.h
+++ file_io/main.h
@@ -4,13 +4,9 @@
 #include <stddef.h>
 #include <unistd.h>
 
-/* Prototype for 0-read_textfile.c */
 ssize_t read_textfile(const char *filename, size_t letters);
-
-/* Prototype for 1-create_file.c */
 int create_file(const char *filename, char *text_content);
-
-/* Prototype for _putchar if used elsewhere */
+int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
 
 #endif /* MAIN_H */
