0054c5a8abe64d6b3a0204de2a4ec6437c35f86f
Aman-Kasa <a.kasa@alustudent.com>
2025-11-10 19:04:31 +0200

    1-create_file.c added

--- file_io/main.h
+++ file_io/main.h
@@ -2,9 +2,16 @@
 #define MAIN_H
 
 #include <stddef.h>
-#include <sys/types.h>
+#include <unistd.h>
 
+/* Prototype for 0-read_textfile.c */
 ssize_t read_textfile(const char *filename, size_t letters);
+
+/* Prototype for 1-create_file.c */
+int create_file(const char *filename, char *text_content);
+
+/* Prototype for _putchar if used elsewhere */
+int _putchar(char c);
 
 #endif /* MAIN_H */
 
